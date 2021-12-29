7262c8f1f7649af7808ac048acd7ca2ad184de55
Sang Hyeok Han <contea95@naver.com>
2021-12-30 02:24:09 +0900

    init: ADD Package(Alamofire, KingFisher) using SPM and configure Cocoapods Package Manager

---
+++ SGS_NCNS/SGS_NCNS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SGS_NCNS
+//
+//  Created by 한상혁 on 2021/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SGS_NCNS/SGS_NCNS/SGS_NCNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGS_NCNSApp.swift
+//  SGS_NCNS
+//
+//  Created by 한상혁 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SGS_NCNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
