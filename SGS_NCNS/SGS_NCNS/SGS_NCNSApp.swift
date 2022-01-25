be34cd8a77b8d343ca5116e3df11a102a57f8c9b
Sang Hyeok Han <contea95@naver.com>
2022-01-25 15:33:08 +0900

    fix: Merge LoginView and ContentView, Use EnvironmentObject

--- SGS_NCNS/SGS_NCNS/SGS_NCNSApp.swift
+++ SGS_NCNS/SGS_NCNS/SGS_NCNSApp.swift
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
+// 앱 전반에서 사용하는 변수 여기서 관리
+class MySettings: ObservableObject {
+    // 프로필 = 무조건 true, 유저검색 -> 자신 : true, 다른사람: false
+    struct UserCheck {
+        var isMe: Bool!
+    }
+    @Published var checkUser: UserCheck = UserCheck(isMe: true)
+}
+var isCurrentUser = MySettings()
+
 @main
 struct SGS_NCNSApp: App {
+    // 로그인 화면에서 로그인 성공 시 이동
+    @State var isLogin = false
     var body: some Scene {
         WindowGroup {
-            ContentView()
-//            LoginView()
+            if !isLogin {
+                LoginView(isLogin: $isLogin)
+            } else {
+                ContentView().environmentObject(isCurrentUser)
+            }
         }
     }
 }
