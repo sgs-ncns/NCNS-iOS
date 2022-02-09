42e176cef9833b4023cf70186bb8a2dd6c949c4d
Sang Hyeok Han <contea95@naver.com>
2022-02-09 21:53:10 +0900

    bulid: Add Amplify Settings

---
+++ SGS_NCNS/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "sgsncns19976f90": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger8f26c83a": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "sgsncns": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
