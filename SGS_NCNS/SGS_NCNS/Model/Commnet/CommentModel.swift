55e90cba786af57b2acb7f0605ae9c098155cf79
Sang Hyeok Han <contea95@naver.com>
2022-02-12 20:30:32 +0900

    chore: Change Model Foldering

--- SGS_NCNS/SGS_NCNS/Model/Commnet/CommentModel.swift
+++ SGS_NCNS/SGS_NCNS/Model/Commnet/CommentModel.swift
@@ -1,15 +1,15 @@
 //
-//  FeedContentModel.swift
+//  CommentModel.swift
 //  SGS_NCNS
 //
 //  Created by 한상혁 on 2022/02/02.
 //
 
 import SwiftUI
-
-enum FeedContent<T: View> {
+// 자른 Feed Content를 뷰로 만들어 이벤트 처리하기 위한 모델
+enum Comment<T: View> {
     case user(NavigationLink<T, ProfileSubView>)
-    case content(NavigationLink<T, CommentView>)
+    case content(T)
     case hashtag(NavigationLink<T, HashTagView>)
     case humantag(NavigationLink<T, ProfileSubView>)
 }
--- SGS_NCNS/SGS_NCNS/Model/Feed/FeedContentModel.swift
+++ SGS_NCNS/SGS_NCNS/Model/Feed/FeedContentModel.swift
@@ -1,15 +1,15 @@
 //
-//  CommentModel.swift
+//  FeedContentModel.swift
 //  SGS_NCNS
 //
 //  Created by 한상혁 on 2022/02/02.
 //
 
 import SwiftUI
-
-enum Comment<T: View> {
+// 자른 Feed Content를 뷰로 만들어 이벤트 처리하기 위한 모델
+enum FeedContent<T: View> {
     case user(NavigationLink<T, ProfileSubView>)
-    case content(T)
+    case content(NavigationLink<T, CommentView>)
     case hashtag(NavigationLink<T, HashTagView>)
     case humantag(NavigationLink<T, ProfileSubView>)
 }
